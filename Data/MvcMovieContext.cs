53b6d179ca799f9fcf4d4768846392ff00fd814e
YongqingYu-qq <62084238+YongqingYu-qq@users.noreply.github.com>
2020-03-12 10:17:48 +0800

    Update MvcMovieContext.cs

--- Data/MvcMovieContext.cs
+++ Data/MvcMovieContext.cs
@@ -12,37 +12,7 @@ namespace WebApplication_Core_mvc.Data
         public MvcMovieContext(DbContextOptions<MvcMovieContext> options)
             : base(options)
         {
-        }
-
-        //protected override void OnModelCreating(ModelBuilder modelBuilder)
-        //{ 
-        //    base.OnModelCreating(modelBuilder);
-        //}
-
-        //protected override void OnModelCreating(DbModelBuilder modelBuilder) { 
-        //    modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
-        //}
-        public DbSet<Creation> Creations { get; set; }
-        public DbSet<CreationLike> CreationLikes { get; set; }
-        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
-        public DbSet<Class1> Class1s { get; set; }
-        public DbSet<Class2> Class2s { get; set; }
-        public DbSet<ViewModel1> ViewModel1 { get; set; }
-        public DbSet<Consignment> Consignment { get; set; }
-        public DbSet<ConsignmentLine> ConsignmentLine { get; set; }
-        public DbSet<Position> Position { get; set; }
-        public DbSet<TaskProjectList> TaskProjectList { get; set; }
-        public DbSet<TaskTickets> TaskTickets { get; set; }
-        public DbSet<TaskCreate> TaskCreate { get; set; }
-
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder
-                .Entity<TaskCreate>(eb =>
-                {
-                    eb.HasNoKey();
-                });
-        }
+        } 
+        public DbSet<Position> Position { get; set; } 
     }
 }
